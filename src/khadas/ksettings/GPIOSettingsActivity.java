de61e0bb9cef599cbcf65f77cd86a83fb9fb8bca
wwJustinB <justinb@wicked-witch.com.au>
2024-01-01 16:26:47 +1100

    convert gpio option to a popup

--- src/khadas/ksettings/GPIOSettingsActivity.java
+++ src/khadas/ksettings/GPIOSettingsActivity.java
@@ -1,10 +1,16 @@
 package com.khadas.ksettings;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,6 +22,11 @@ import java.util.List;
 
 public class GPIOSettingsActivity extends PreferenceActivity {
 
+    private Dialog dialog;
+    Button actionButton;
+    Button cancelButton;
+    EditText textInput;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.gpio_settings_control);
@@ -23,14 +34,14 @@ public class GPIOSettingsActivity extends PreferenceActivity {
         // Export Pin Preference
         Preference exportPinPref = findPreference("export_pin");
         exportPinPref.setOnPreferenceClickListener(preference -> {
-            exportPin();
+            showDialog(getBaseContext(),true);
             return true;
         });
 
         // Unexport Pin Preference
         Preference unexportPinPref = findPreference("unexport_pin");
         unexportPinPref.setOnPreferenceClickListener(preference -> {
-            unexportPin();
+            showDialog(getBaseContext(),false);
             return true;
         });
 
@@ -39,8 +50,6 @@ public class GPIOSettingsActivity extends PreferenceActivity {
         if (gpioPinsPreference != null) {
             gpioPinsPreference.setLayoutResource(R.layout.recycler_preference_layout);
         }
-
-
     }
 
     private void exportPin() {
@@ -62,4 +71,60 @@ public class GPIOSettingsActivity extends PreferenceActivity {
                 .getString("pin_number", "0");
         return Integer.parseInt(pinString);
     }
+
+    public void showDialog(Context context,boolean exportMode) {
+        this.dialog = new Dialog(context);
+        this.dialog.setContentView(exportMode ? R.layout.export_popup_layout : R.layout.unexport_popup_layout);
+
+        initializeDialogViews(exportMode);
+
+        dialog.setOnDismissListener(dialogInterface -> {
+            // Handle the dismissal
+            onDialogDismissed();
+        });
+
+        dialog.show();
+    }
+
+    private void onDialogDismissed() {
+    }
+
+    private void initializeDialogViews(boolean exportMode) {
+        textInput = dialog.findViewById(R.id.text_popupinput);
+
+        actionButton = dialog.findViewById(R.id.button_action);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textInput.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getBaseContext(), "Please enter a pin number", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    if (dialog.isShowing())
+                    {
+                        dialog.dismiss();
+                    }
+                    if (exportMode)
+                    {
+                        exportPin();
+                    } else
+                    {
+                        unexportPin();
+                    }
+                }
+            }
+        });
+        cancelButton = dialog.findViewById(R.id.button_cancel);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dialog.isShowing()) {
+                    dialog.dismiss();
+                }
+            }});
+    }
+
+
 }
