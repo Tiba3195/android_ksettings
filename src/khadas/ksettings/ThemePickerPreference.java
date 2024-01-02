7defbfef30ab46a0d0e7a28a194875f7c279bad4
wwJustinB <justinb@wicked-witch.com.au>
2024-01-02 17:26:28 +1100

    refactor theme picker pref

--- src/khadas/ksettings/ThemePickerPreference.java
+++ src/khadas/ksettings/ThemePickerPreference.java
@@ -8,18 +8,21 @@ import android.view.View;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.khadas.ksettings.views.ThemePicker;
 import com.khadas.util.GPIOUtils;
 
 public class ThemePickerPreference extends Preference {
 
+    private ThemePicker themePicker;
     public ThemePickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setLayoutResource(R.layout.theme_picker_layout); // Set your custom layout here
+
+        setLayoutResource(R.layout.theme_picker_holder); // Set your custom layout here
     }
 
     @Override
     protected void onBindView(View view) {
         super.onBindView(view);
-
+        themePicker = (ThemePicker) view.findViewById(R.id.theme_picker_view);
     }
 }
