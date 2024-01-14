fdb5bd4e6d6377759253583d6ea45f702a462590
Tiba3195 <justin_beast@hotmail.com>
2024-01-15 01:23:33 +1100

    Update Android.mk

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libjni_ksettings
 
-LOCAL_SRC_FILES := /ksettings.cpp
+LOCAL_SRC_FILES := ksettings.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes \
 	prebuilts/jdk/jdk8/linux-x86/include \
@@ -30,11 +30,12 @@ LOCAL_CPPFLAGS += -std=c++11 # Adjust as needed for your C++ standard
 
 LOCAL_LDLIBS := -llog -ldl
 
-LOCAL_MODULE_RELATIVE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 
 LOCAL_TARGET_ARCH_ABI := arm64-v8a
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_PLATFORM_APIS := true
 
 include $(BUILD_SHARED_LIBRARY)
 
