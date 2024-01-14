1e0477a7b9ba161d637a58f3492f7ddff7c01871
Tiba3195 <justin_beast@hotmail.com>
2024-01-14 22:23:35 +1100

    Update Android.mk

--- src/jni/Android.mk
+++ src/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libjni_ksettings
 
-LOCAL_SRC_FILES := ksettings.cpp
+LOCAL_SRC_FILES := /ksettings.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/includes \
 	prebuilts/jdk/jdk8/linux-x86/include \
@@ -15,6 +15,8 @@ LOCAL_SHARED_LIBRARIES := \
     libdl \
     libgpiocontrol \
 	libnativehelper
+
+LOCAL_PRIVATE_MODULE_LIBRARIES := libgpiocontrol
 
 LOCAL_CFLAGS += \
     -ffast-math \
@@ -28,9 +30,11 @@ LOCAL_CPPFLAGS += -std=c++11 # Adjust as needed for your C++ standard
 
 LOCAL_LDLIBS := -llog -ldl
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_RELATIVE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 
 LOCAL_TARGET_ARCH_ABI := arm64-v8a
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_SHARED_LIBRARY)
 
